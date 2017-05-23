deee788818cf78eb49137fd976c6154fc5f124ea
Thibault Charbonnier <thibaultcha@me.com>
2017-05-23 15:16:15 -0700

    feat(conf) new properties for datastore cache
    4 new properties now define the behavior of the new invalidation events
    framework, and configuration entities caching. Their role and behavior
    is detailed in this commit, as part of the default configuration file.

--- kong/templates/kong_defaults.lua
+++ kong/templates/kong_defaults.lua
@@ -59,6 +59,11 @@ cassandra_repl_factor = 1
 cassandra_data_centers = dc1:2,dc2:3
 cassandra_schema_consensus_timeout = 10000
 
+db_update_frequency = 5
+db_update_propagation = 0
+db_cache_ttl = 3600
+db_cache_miss_ttl = 300
+
 dns_resolver = NONE
 dns_hostsfile = /etc/hosts
 
